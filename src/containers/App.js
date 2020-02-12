8536d5823354f9ca4fd690c40a1ac49fc0c9f9f2
gautambitious <mvgt.gautam@gmail.com>
2020-02-12 10:52:59 +0530

    Welcome Screen

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,39 @@
 import React, {Component} from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
+import Welcome from "../components/Welcome.js";
 
-class App extends Component{
-  render(){
-  return  (
-     <h1> Hello </h1>
-   );
+const particleParams={
+  particles: {
+      number: {
+        value: 200,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    }
   }
-}
+class App extends Component{
 
+  constructor(){
+    super();
+    this.state = {
+      route : "welcome"
+    }
+  }
+
+
+  render(){
+    if(this.state.route == "welcome"){
+      return (
+        <div className="App">
+        <Particles
+                className='particles'
+                params={particleParams}
+                />
+          <Welcome />
+         </div>
+      );
+    }}}
 export default App;
